8fc9ac881930196e4282bd45f98c1abeff45b471
wesandmarlene <wesley.d.gibbs@me.com>
2019-04-11 19:37:24 -0400

    Create create-intermediate-certificate.sh

---
+++ create-intermediate-certificate.sh
@@ -0,0 +1,8 @@
+cd /root/ca
+
+openssl ca -config openssl.cnf -extensions v3_intermediate_ca \
+  -days 3650 -notext -md sha256 \
+  -in intermediate/csr/intermediate.csr.pem \
+  -out intermediate/certs/intermediate.cert.pem
+
+chmod 444 intermediate/certs/intermediate.cert.pem
