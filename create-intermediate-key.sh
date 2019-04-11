6b232aeb85a0c8c457c3490d47ac3ce60c46e30d
wesandmarlene <wesley.d.gibbs@me.com>
2019-04-11 19:38:32 -0400

    Update create-intermediate-key.sh

--- create-intermediate-key.sh
+++ create-intermediate-key.sh
@@ -1,8 +1,6 @@
 cd /root/ca
+
 openssl genrsa -aes256 \
   -out intermediate/private/intermediate.key.pem 4096
-
-# Enter pass phrase for intermediate.key.pem: secretpassword
-# Verifying - Enter pass phrase for intermediate.key.pem: secretpassword
 
 chmod 400 intermediate/private/intermediate.key.pem
