beacde476bca5421348c7dd786d8a8755fee964d
wesandmarlene <wesley.d.gibbs@me.com>
2019-04-11 20:22:55 -0400

    Delete create-certificate.sh

--- scripts/usr_cert/create-certificate.sh
+++
@@ -1,6 +0,0 @@
-cd /root/ca
-
-openssl ca -config intermediate/openssl.cnf \
-  -extensions usr_cert -notext -md sha256 \
-  -in intermediate/csr/bob@example.com.csr.pem \
-  -out intermediate/certs/bob@example.com.cert.pem
